a4c9a4939381cd16a10637f5d817c64dd1129399
noye <noy.elankry@gmail.com>
2023-07-30 23:01:01 +0300

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import About from "./components/About"
 import Skills from "./components/Skills"
 import Work from "./components/Work"
 import Contact from "./components/Contact"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <Home />
+            <About />
+            <Skills />
+            <Work />
+            <Contact />
+          </div>
+        } />
+      </Routes>
     </div>
   );
 }
